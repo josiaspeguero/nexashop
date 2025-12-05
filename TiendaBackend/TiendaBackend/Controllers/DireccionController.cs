d57e4344023850d1c7dcbbbee407b6a7f24c684a
josiaspegueroltjnc <josiaspegueroltjnc@gmail.com>
2025-12-05 14:07:24 -0400

    feat: direcciones controller agregado

---
+++ TiendaBackend/TiendaBackend/Controllers/DireccionController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TiendaBackend.Application.UseCases;
+using TiendaBackend.Domain.Models;
+
+namespace TiendaBackend.Controllers
+{
+    [Route("api/direcciones")]
+    [ApiController]
+    public class DireccionController : ControllerBase
+    {
+        private readonly AgregarDireccionEnvio _agregarDireccion;
+        private readonly ActualizarDireccionEnvio _actualizarDireccion;
+
+        public DireccionController(AgregarDireccionEnvio agregarDireccion, ActualizarDireccionEnvio actualizarDireccion)
+        {
+            _agregarDireccion = agregarDireccion;
+            _actualizarDireccion = actualizarDireccion;
+        }
+
+        [HttpPost("agregar-direccion")]
+        public async Task<ActionResult> AgregarDireccion(Direccion direccion)
+        {
+            var result = await _agregarDireccion.AgregarDireccionTask(direccion);
+            if (!result.status)
+            {
+                return BadRequest(result.message);
+            }
+            return Ok(result.message);
+        }
+
+        [HttpPut("actualizar-direccion")]
+        public async Task<ActionResult> ActualizarDireccion(Direccion direccion)
+        {
+            var result = await _actualizarDireccion.ActualizarDireccionTask(direccion);
+            if (!result.status)
+            {
+                return BadRequest(result.message);
+            }
+            return Ok(result.message);
+        }
+    }
+
+}
