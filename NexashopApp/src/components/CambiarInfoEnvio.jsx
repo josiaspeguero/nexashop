cdad740ddcc75c644a1232e3829e9498bbc00dda
josiaspegueroltjnc <josiaspegueroltjnc@gmail.com>
2025-12-05 23:19:36 -0400

    feat: cambiar direccion de envio

---
+++ NexashopApp/src/components/CambiarInfoEnvio.jsx
@@ -0,0 +1,32 @@
+import "../styles/components.css";
+import { FaTimes } from "react-icons/fa";
+
+function CambiarInfoEnvio() {
+  return (
+    <div>
+      <div className="modal-container " id="modal-container">
+        <div className="modal">
+          <FaTimes
+            className="icon"
+            onClick={() =>
+              document
+                .getElementById("modal-container")
+                .classList.remove("show")
+            }
+          />
+          <h2>Cambia tu direccion de envio</h2>
+          <label htmlFor="">Direccion Actual</label>
+          <input
+            type="text"
+            placeholder="Calle #04, esq. Sabala, San Jose de Ocoa"
+          />
+          <label htmlFor="">Codigo Postal</label>
+          <input type="text" placeholder="98000" />
+          <button type="button">Actualizar</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CambiarInfoEnvio;
