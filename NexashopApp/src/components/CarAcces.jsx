9340112ea14e15bda42dc53824e03d97a4f7a1bf
josiaspegueroltjnc <josiaspegueroltjnc@gmail.com>
2025-12-08 00:14:19 -0400

    feat: acceso carrito de compras desde toda la pagina

--- NexashopApp/src/components/CarAcces.jsx
+++ NexashopApp/src/components/CarAcces.jsx
@@ -1,14 +1,15 @@
 import { FaShoppingCart } from "react-icons/fa";
+import {Link} from 'react-router-dom'
 
 function CarAcces() {
   return (
     <div>
-      <div className="car-access">
+      <Link to="/cart" className="car-access">
         <div className="car-elements">
           <FaShoppingCart className="icon" />
           <p className="car-items">10</p>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
