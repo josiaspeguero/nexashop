afd66326d4cf02a070ac6233848dc6823ebe705a
josiaspegueroltjnc <josiaspegueroltjnc@gmail.com>
2025-12-14 13:32:57 -0400

    feat: eliminar producto

---
+++ TiendaBackend/TiendaBackend/Application/UseCases/EliminaProductoDelCarrito.cs
@@ -0,0 +1,23 @@
+﻿using TiendaBackend.Domain.Interfaces;
+
+namespace TiendaBackend.Application.UseCases
+{
+    public class EliminaProductoDelCarrito
+    {
+        private readonly IOrdenProductoRepository _ordenProducto;
+
+        public EliminaProductoDelCarrito(IOrdenProductoRepository ordenProducto)
+        {
+            _ordenProducto = ordenProducto;
+        }
+        public async Task<(bool status, string message)> eliminarOrdenTask(int id)
+        {
+            var res = await _ordenProducto.EliminarOrdenAsync(id);
+            if (!res)
+            {
+                return (false, "No se pudo eliminar");
+            }
+            return (true, "producto eliminado");
+        }
+    }
+}
