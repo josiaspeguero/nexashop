af3f425f771693a34da589415b5cf36cfdc880a5
josiaspegueroltjnc <josiaspegueroltjnc@gmail.com>
2025-12-14 17:35:56 -0400

    feat: actualizar informacion de usuario

--- NexashopApp/src/pages/Profile.jsx
+++ NexashopApp/src/pages/Profile.jsx
@@ -1,13 +1,30 @@
 import { FaUser, FaUpload, FaPlus, FaTruck } from "react-icons/fa";
 import "../styles/profile.styles.css";
 import BackHome from "../components/ui/BackHome";
-import {ToastContainer, toast} from 'react-toastify'
+import { ToastContainer, toast } from "react-toastify";
+import { useEffect, useState } from "react";
 
 function Profile() {
+  const [info, setInfo] = useState({
+    nombreCompleto: "",
+    correo: "",
+    numero: "8093456789",
+  });
+  useEffect(() => {
+    const usuario = localStorage.getItem("usuario");
+    if (!usuario) {
+      return;
+    }
+    const usuarioParse = JSON.parse(usuario);
+    setInfo({
+      nombreCompleto: usuarioParse.nombreCompleto,
+      correo: usuarioParse.correo,
+    });
+  }, []);
   return (
     <div>
       <BackHome />
-      <ToastContainer/>
+      <ToastContainer position="bottom-right" />
       <div className="profile-container">
         <div className="profile-title">
           <p>Account</p>
@@ -45,55 +62,14 @@ function Profile() {
                 <span>Change or edit your personal info in some click</span>
               </div>
             </div>
-
-            {/* <div className="menu-item">
-              <div className="line-decoration"></div>
-              <div className="icon-container">
-                <FaTruck className="icon" />
-              </div>
-              <div className="description">
-                <p>Your Directions</p>
-                <span>Change or edit your directions in some click</span>
-              </div>
-            </div> */}
-            {/* <div className="menu-item">
-              <div className="line-decoration"></div>
-              <div className="icon-container">
-                <FaLock className="icon" />
-              </div>
-              <div className="description">
-                <p>Security</p>
-                <span>Change or edit your security settings in some click</span>
-              </div>
-            </div> */}
-            {/* <div className="menu-item">
-              <div className="line-decoration"></div>
-              <div className="icon-container">
-                <FaSlidersH className="icon" />
-              </div>
-              <div className="description">
-                <p>Preferences</p>
-                <span>Change or edit your preferences in some click</span>
-              </div>
-            </div> */}
-            {/* <div className="menu-item">
-              <div className="line-decoration"></div>
-              <div className="icon-container">
-                <FaCog className="icon" />
-              </div>
-              <div className="description">
-                <p>Settings</p>
-                <span>Change or edit your settings in some click</span>
-              </div>
-            </div> */}
           </div>
 
           <div className="profile-info profile-section">
             <div className="profile-header">
               <h2>Personal Information</h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Laudantium esse Lorem, ipsum dolor.
+                Manage your personal details, including your full name, contact
+                number, and email.
               </p>
             </div>
             <hr />
@@ -107,9 +83,12 @@ function Profile() {
                     <span>PNG, JPG, WEBP</span>
                   </div>
 
-                  <button className="upload-profile" onClick={()=>{
-                    toast.error("Funcion no disponible actualmente")
-                  }}>
+                  <button
+                    className="upload-profile"
+                    onClick={() => {
+                      toast.warning("Funcion No Disponible Actualmente");
+                    }}
+                  >
                     <FaUpload className="icon" />
                     Upload Image
                   </button>
@@ -120,17 +99,30 @@ function Profile() {
             <div className="info-section">
               <div className="title-info">Basic Info</div>
               <div className="input-group">
-                <input type="text" placeholder="josias peguero" />
-                <input type="text" placeholder="+1 (809) 391 4895" />
+                <input
+                  type="text"
+                  placeholder={info.nombreCompleto}
+                  value={info.nombreCompleto}
+                />
+                <input
+                  type="text"
+                  placeholder="+1 (809) 391 4895"
+                  value={info.numero}
+                />
               </div>
             </div>
             <hr />
             <div className="info-section">
               <div className="title-info">Your Emails</div>
-              <input type="text" placeholder="josiaspegueroltjnc@gmail.com" />
-              <div className="add-email">
+              <input type="text" value={info.correo} />
+              <div
+                className="add-email"
+                onClick={() => {
+                  toast.warning("Función No Disponible Actualmente");
+                }}
+              >
                 <FaPlus className="icon" />
-                <p>Add Another Email</p>
+                <p>Update Information</p>
               </div>
             </div>
           </div>
